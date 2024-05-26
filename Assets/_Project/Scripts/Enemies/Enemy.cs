028345f4cd3b31e685e886bca43fce6236632474
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-05-26 08:33:14 +0530

    Added Navmesh and TMPro Packages + Made Enemy AI with Health And Movement

---
+++ Assets/_Project/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : Health
+{
+    [Header("Movement Settings")]
+    public float stoppingDistance = 2.5f;
+    public float moveSpeed = 4f;
+    public float chaseRange = 7f; // Range within which the enemy will start chasing the player
+
+    [Header("Damage Settings")]
+    public float attackRate;
+    public float damage;
+    public float damageDistance;
+
+    [Header("UI Settings")]
+    [SerializeField] Slider healthBar;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        if (healthBar)
+        {
+            healthBar.minValue = 0;
+            healthBar.maxValue = maxHealth;
+            healthBar.value = maxHealth;
+        }
+    }
+
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+
+        if (healthBar) healthBar.value = currentHealth;
+    }
+}
---
+++ Assets/_Project/Scripts/Enemies/Enemy_AIMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy_AIMovement : MonoBehaviour
+{
+    Enemy enemyCS;
+    NavMeshAgent navAgent;
+    float distanceToPlayer;
+
+    void Start()
+    {
+        enemyCS = GetComponent<Enemy>();
+        navAgent = GetComponent<NavMeshAgent>();
+
+        navAgent.speed = enemyCS.moveSpeed;
+        navAgent.stoppingDistance = enemyCS.stoppingDistance;
+    }
+
+    void Update()
+    {
+        // Calculate the distance to the player
+        distanceToPlayer = Vector3.Distance(Player.Instance.transform.position, transform.position);
+
+        // Check if the player is within chase range
+        if (distanceToPlayer <= enemyCS.chaseRange)
+        {
+            // Set the agent's destination to the player's position
+            navAgent.SetDestination(Player.Instance.transform.position);
+        }
+        else
+        {
+            // Stop the agent from moving if the player is out of range
+            navAgent.ResetPath();
+        }
+    }
+}
