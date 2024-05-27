619a5c98dfcc8dcc7b5efe9a259776afe36c516c
Max Davidse <m.davidse@rubicon.nl>
2024-05-27 07:00:43 +0200

    Initial changes to the tower system

--- Assets/_Project/Scripts/Tower/Tower.cs
+++ Assets/_Project/Scripts/Tower/Tower.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tower : MonoBehaviour
+public class Tower : Health
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static Tower Instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Relic settings")] 
+    [SerializeField] int _damage = 100;
+    [SerializeField] float _fireRate = 0.05f;
+    [SerializeField] GameObject _bulletPrefab;
+    [SerializeField] Transform _firePoint;
+    [SerializeField] float _projectileSpeed = 20f;
+    [SerializeField] float _bulletLife = 0.5f;
+
+    [Header("Attack settings")] 
+    [SerializeField] float _attackRadius = 20f;
 }
